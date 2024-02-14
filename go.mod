d800b7058db7b457e5a97d3a882fca714f8ee83c
koudaiii <cs006061@gmail.com>
2024-02-14 15:42:05 +0900

    Add feature flag

--- go.mod
+++ go.mod
@@ -2,13 +2,17 @@ module gostart
 
 go 1.19
 
-require github.com/gin-gonic/gin v1.9.1
+require (
+	github.com/gin-gonic/gin v1.9.1
+	github.com/open-feature/go-sdk v1.10.0
+)
 
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-logr/logr v1.4.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
@@ -24,9 +28,10 @@ require (
 	github.com/ugorji/go/codec v1.2.11 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/exp v0.0.0-20240205201215-2c58cdc269a3 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,22 +1,39 @@
 package main
 
 import (
-    "github.com/gin-gonic/gin"
-    "net/http"
+	"context"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/open-feature/go-sdk/openfeature"
 )
 
 const defaultMessage = "Hello!"
+const newWelcomeMessage = "Hello, welcome to this OpenFeature-enabled website!"
 
 func main() {
-    // Initialize Go Gin
-    engine := gin.Default()
+	// Initialize OpenFeature client
+	client := openfeature.NewClient("GoStartApp")
 
-    // Setup a simple endpoint
-    engine.GET("/hello", func(c *gin.Context) {
-        c.JSON(http.StatusOK, defaultMessage)
-        return
+	// Initialize Go Gin
+	engine := gin.Default()
 
-    })
+	// Setup a simple endpoint
+	engine.GET("/hello", func(c *gin.Context) {
 
-    engine.Run()
+		// Evaluate welcome-message feature flag
+		welcomeMessage, _ := client.BooleanValue(
+			context.Background(), "welcome-message", false, openfeature.EvaluationContext{},
+		)
+
+		if welcomeMessage {
+			c.JSON(http.StatusOK, gin.H{"message": newWelcomeMessage})
+			return
+		} else {
+			c.JSON(http.StatusOK, gin.H{"message": defaultMessage})
+			return
+		}
+	})
+
+	engine.Run()
 }
