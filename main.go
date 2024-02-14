8ba6edf2941b525a5ce57c100ad3cd73d64aedc0
koudaiii <cs006061@gmail.com>
2024-02-14 16:06:04 +0900

    using flagd provider

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	flagd "github.com/open-feature/go-sdk-contrib/providers/flagd/pkg"
 	"github.com/open-feature/go-sdk/openfeature"
 )
 
@@ -12,6 +13,9 @@ const defaultMessage = "Hello!"
 const newWelcomeMessage = "Hello, welcome to this OpenFeature-enabled website!"
 
 func main() {
+	// Use flagd as the OpenFeature provider
+	openfeature.SetProvider(flagd.NewProvider())
+
 	// Initialize OpenFeature client
 	client := openfeature.NewClient("GoStartApp")
 
